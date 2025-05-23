2745b3e8d1cc5317de0a8155113e4e74c357cc86
Eivind Hagen <eivind.hagen@frisbii.com>
2025-05-23 11:02:38 +0200

    Fix work log query

--- src/main/java/com/timesync/AbstractDto.java
+++ src/main/java/com/timesync/AbstractDto.java
@@ -2,8 +2,15 @@ package com.timesync;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public abstract class AbstractDto implements NativeSerializable {
+
+    @Override
+    public String toString() {
+        return new ReflectionToStringBuilder(this)
+                .toString();
+    }
 }
